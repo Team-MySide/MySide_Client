d13d88420297c856c26d76e67845bc0a87e9e41a
larsien <larsien85@gmail.com>
2021-08-16 22:25:12 +0900

    Refactor : change temp token

--- lib/common/UserProfile.dart
+++ lib/common/UserProfile.dart
@@ -2,5 +2,5 @@ class UserProfile {
   static bool isLogin = false;
   static String token = "";
   static const String TEMP_TOKEN =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjgsImlhdCI6MTYyMjk4NDU4MywiZXhwIjoxNjI0MTk0MTgzLCJpc3MiOiJpZyJ9.KBi6Egq_QoqQ11YD3cUnlKPp0YGlgvO-Po5JY22_nBs";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI5MTE4MTk0LCJleHAiOjE2MzAzMjc3OTQsImlzcyI6ImlnIn0.WBrJLaWKpy4_090_jXw-nYQ4sMfFdGB-yYP_eN1TkCc";
 }
