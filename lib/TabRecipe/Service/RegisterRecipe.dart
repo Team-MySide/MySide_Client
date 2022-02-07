560885b1f11d1627690b50dffff0b1d50034dfce
larsien <larsien85@gmail.com>
2022-02-07 23:49:56 +0900

    Feature : 레시피등록 api 샘플

---
+++ lib/TabRecipe/Service/RegisterRecipe.dart
@@ -0,0 +1,26 @@
+import 'package:my_side_client/common/MySideConnect.dart';
+import 'package:my_side_client/common/UserProfile.dart';
+
+abstract class IFetch {
+  void registerRecipe();
+}
+
+//wiki link
+class RegisterRecipeService extends MySideConnect implements IFetch {
+  @override
+  Future<bool> registerRecipe() async {
+    httpClient.baseUrl = "http://54.180.67.217:3000";
+
+    //모든 헤더에 붙인다.
+    httpClient.addRequestModifier((request) {
+      request.headers['Content-Type'] = 'application/json';
+      request.headers['token'] = UserProfile.TEMP_TOKEN;
+      return request;
+    });
+    var resp = await get(""); //request path
+    if (resp.statusCode == 200) {
+      return true; //pass
+    }
+    return false; //fail
+  }
+}
